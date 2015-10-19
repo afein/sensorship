a3734e270e9b5feb324cd01c8f82f50ea256e07c
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-10-19 16:03:05 -0700

    Added REST API endpoints for master agent

--- master_agent/rest.py
+++ master_agent/rest.py
@@ -1,1 +1,27 @@
-import flask
+from flask import Flask
+import json
+
+app = Flask("master-agent")
+
+@app.route("/submit", methods=["POST"])
+def submit_job():
+    resp = {"something": "implemented"}
+    return json.dumps(resp)
+
+@app.route("/on", methods=["POST"])
+def start_job():
+    resp = {"something": "implemented"}
+    return json.dumps(resp)
+
+@app.route("/off", methods=["POST"])
+def stop_job():
+    resp = {"something": "implemented"}
+    return json.dumps(resp)
+
+@app.route("/register", methods=["POST"])
+def register_node():
+    resp = {"something": "implemented"}
+    return json.dumps(resp)
+
+if __name__ == "__main__":
+        app.run()
