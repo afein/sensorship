c7a94f195999b6de84c3a8f1feea040c5faf1080
Alex Mavrogiannis <nalfemp@gmail.com>
2015-10-19 16:44:51 -0700

    added datapipe holder to vnm

--- node_agent/virtual_network_manager.py
+++ node_agent/virtual_network_manager.py
@@ -2,7 +2,9 @@ import docker
 from sensor_formatter import SensorDataFormatter
 
 class VirtualNetworkManager(object):
+    
     def __init__(self):
+        self.datapipes = []
         pass
 
     def create_datapipe(self, type, host, port, sensor, interval):
