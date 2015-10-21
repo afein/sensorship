77468b9e3434059e0130e7c06f8474ecc11ee9db
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-10-21 15:50:13 -0700

    Implemented health check service for master, refined Node class

--- lib/node.py
+++ lib/node.py
@@ -1,13 +1,47 @@
+import socket
+import fcntl
+import struct
+
 class Node(object):
-    def __init__(self, id, sensors):
+    def __init__(self, id, sensors, cpu, memory):
         self.id = id
         self.sensors = sensors
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.ip = ipaddr = socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,  # SIOCGIFADDR
+            struct.pack('256s', ifname[:15])
+        )[20:24])
+        self.cpu = None
+        self.memory = None
+        self.healthy = True
 
     def get_id(self):
         return self.id
+
+    def get_ip(self):
+        return self.ip
 
     def get_sensors(self):
         return self.sensors
 
     def set_sensors(self, sensor):
         self.sensors.append(sensor)
+
+    def get_cpu(self):
+        return self.cpu
+
+    def set_cpu(self, cpu):
+        self.cpu = cpu
+
+    def get_memory(self):
+        return self.memory
+
+    def set_memory(self, memory):
+        self.memory = memory
+
+    def is_healthy(self):
+        return self.healthy
+
+    def set_healthy(self, is_healthy):
+        self.healthy = is_healthy
--- master_agent/health_check_service.py
+++ master_agent/health_check_service.py
@@ -1,9 +1,30 @@
+import requests
+import json
+import time
+
 class HealthCheckService(object):
-    def __init__(self):
-        pass
+    def __init__(self, interval, cluster_state):
+        self.interval = interval # in milliseconds
+        self.cluster_state = cluster_state
 
     def get_interval(self):
-        pass
+        return self.interval
 
     def set_interval(self, interval):
-        pass
+        self.interval = interval
+
+    def poll_health_status(self):
+        nodes = self.cluster_state.get_configured_nodes()
+        while True:
+            for node in nodes:
+                health_check_url = "http://" + nodes[node].get_ip() + "/healthz"
+                node_id = node.get_id()
+                resp = requests.get(health_check_url)
+                if resp.status_code != 200:
+                    nodes[node_id].set_healthy = False
+                else:
+                    nodes[node_id].set_healthy = True
+                    data = json.dumps(resp.content)
+                    nodes[node_id].set_cpu(data["cpu"])
+                    nodes[node_id].set_memory(data["memory"])
+            time.sleep(float(self.get_interval() / 1000)) # time.sleep expects the duration in seconds
