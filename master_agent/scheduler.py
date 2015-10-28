7390548957a94398696dd4ba3d5f894a5941e685
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-10-28 15:33:09 -0700

    Completed schedule function for scheduler

--- master_agent/scheduler.py
+++ master_agent/scheduler.py
@@ -14,9 +14,13 @@ class Scheduler(object):
             if state != 'on':
                 raise Exception('Error schedule: task submitted in not in "On" state')
 
-            node = self.policy(user_config)
-            # TODO: call dispatcher to deploy container
-            # upon successful deployment update cluster state
+            node, datapipes = self.policy(user_config)
+            if node_dispatcher.deploy_container(user_config):
+                for datapipe in datapipes:
+                    flag = node_dispatcher.establish_datapipe(node, datapipe['remote_node'], datapipe['sensor'], datapipe['interval'])
+                    if not flag:
+                        raise Exception('Error when establishing datapipe')
+                cluster_state.add_deployed_containers(node)
 
     def greedy(self, user_config):
         image = user_config['image']
@@ -26,18 +30,22 @@ class Scheduler(object):
         for mapping in mappings:
             node = mapping['node']
             if node not in required_sensors:
-                required_sensors[node] = [mapping['sensor']]
+                required_sensors[node] = [{'sensor': mapping['sensor'], 'interval': mapping['interval']}]
             else:
-                required_sensors[node].append(mapping['sensor'])
+                required_sensors[node].append({'sensor': mapping['sensor'], 'interval': mapping['interval']})
         
         node_datapipe_mapping = cluster_state.get_node_datapipe_mapping()
         required_datapipes = {}
+        actual_datapipes = {}
         for current_node in required_sensors:
             count = 0
             for other_node in required_sensors:
                 if other_node != current_node:
                     for sensor in required_sensors[other_node]:
-                        if sensor not in node_datapipe_mapping[current_node][other_node]:
+                        if sensor['sensor'] not in node_datapipe_mapping[current_node][other_node]:
                             count += 1
+                            actual_datapipes[current_node] = {'remote_node': other_node, 'sensor': sensor['sensor'], 'interval': sensor['interval']}
             required_datapipes[current_node] = count
-        return min(required_datapipes, key=required_datapipes.get)
+        scheduled_node = min(required_datapipes, key=required_datapipes.get)
+        scheduled_datapipes = actual_datapipes[scheduled_node]
+        return (scheduled_node, scheduled_datapipes)
