6ab4ad28afd2eea244f4ea608f6069fb32267ec6
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-11-02 18:21:56 -0800

    started implementing the first test for scheduler

--- master_agent/scheduler_test.py
+++ master_agent/scheduler_test.py
@@ -1,12 +1,19 @@
 import unittest
 from scheduler import Scheduler
+from cluster_state import ClusterState
+from node_dispatcher import NodeDispatcher
 
 class SchedulerTestcase(unittest.TestCase):
     def setUp(self):
-        self.scheduler = Scheduler()
+        self.cluster_state = ClusterState()
+        self.node_dispatcher = NodeDispatcher()
+        self.scheduler = Scheduler(cluster_state, node_dispatcher, greedy)
 
-    def test_schedule_X(self):
+    def test_schedule_simple_topology(self):
+        user_config = {'image': 'afein/ubuntussh', 'mappings' : [{'node': 1, 'sensor': 'temperature', 'port': '4000', 'interval': 500}]}
+        node, datapipes = self.scheduler.schedule(user_config)
         self.assertEqual(1, 1)
 
     def tearDown(self):
-        pass
+        self.scheduler.dispose()
+        self.scheduler = None
