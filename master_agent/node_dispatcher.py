bdf6c7da8f3d9e499fa2f21c6f81c2de7c32cc2a
Dimitris Sarlis <sarlis.dim@gmail.com>
2015-11-09 16:57:44 -0800

    Implemented node_dispatcher

--- master_agent/node_dispatcher.py
+++ master_agent/node_dispatcher.py
@@ -1,15 +1,37 @@
+import requests
+import json
+
 class NodeDispatcher(object):
-    def __init__(self):
-        pass
+    def __init__(self, port):
+        self.agent_listen_port = port 
 
-    def establish_datapipe(self, node1, node2, sensor, interval):
-        pass
+    def establish_datapipe(self, node_host, dst_host, sensor, interval):
+        req_payload = json.dumps({'host' : dst_host)
+        r = requests.post('http://%s:%s/remote_datapipe' % node_host, self.agent_listen_port, data=req_payload)
+        if r.status_code = 200:
+            resp_payload = json.load(r.text)
+            req_payload = json.dumps({'host' : dst_host, 'port' : resp_payload, 'sensor' : sensor, 'interval' : interval})
+            r = requests.post('http://%s:%s/datapipe' % node_host, self.agent_listen_port, data=req_payload)
+            return r.status_code
+        return r.status_code
 
-    def destroy_datapipe(self, node1, node2, sensor):
-        pass
+    def destroy_datapipe(self, node_host, dst_host, port, sensor):
+        req_payload = json.dumps({'host' : dst_host, 'port' : port, 'sensor' : sensor})
+        r = requests.delete('http://%s:%s/datapipe' % node_host, self.agent_listen_port, data=req_payload)
+        return r.status_code
 
     def deploy_container(self, host, image, sensor_mappings):
-        pass
+        req_payload = json.dumps({'image' : image, 'ports' : sensor_mappings})
+        r = requests.post('http://%s:%s/container' % host, self.agent_listen_port, data=req_payload)
+        if r.status_code == 200:
+            resp_payload = json.loads(r.text)
+            container_id = resp_payload['container_id']
+            port_bindings = resp_payload['port_bindings']
+            return (container_id, port_bindings)
+        return (None, None)
+
 
     def stop_container(self, host, ID):
-        pass
+        req_payload = json.dumps({'image' : image})
+        r = requests.delete('http://%s:%s/container' % host, self.agent_listen_port, data=req_payload)
+        return r.status_code
