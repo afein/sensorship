3cd8313f1963645bcc4b8b426dc409648b0a8a96
Alex Mavrogiannis <nalfemp@gmail.com>
2015-11-23 18:18:09 -0800

    added skeleton for star wars image

---
+++ star_wars_image/server.py
@@ -0,0 +1,43 @@
+import threading
+import time
+import SocketServer
+import json
+import wave
+
+class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
+
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print "%s wrote: " % self.client_address[0]
+        print self.data
+        reading = json.loads(self.data)
+
+        if "sensor" in reading and "value" in reading and \
+            reading["sensor"] == "GroveButton" and reading["value"] == 1:
+                wave.open("/src/starwars.wav")
+
+        self.request.send(self.data.upper())
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+if __name__ == "__main__":
+
+    HOST = ''
+    PORT_A = 1234
+    PORT_B = 1235
+
+    server_A = ThreadedTCPServer((HOST, PORT_A), ThreadedTCPRequestHandler)
+    server_B = ThreadedTCPServer((HOST, PORT_B), ThreadedTCPRequestHandler)
+
+    server_A_thread = threading.Thread(target=server_A.serve_forever)
+    server_B_thread = threading.Thread(target=server_B.serve_forever)
+
+    server_A_thread.setDaemon(True)
+    server_B_thread.setDaemon(True)
+
+    server_A_thread.start()
+    server_B_thread.start()
+
+    while 1:
+        time.sleep(1)
